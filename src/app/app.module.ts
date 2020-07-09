75ed4a2abb40fe91ba2b2b6e1c43494bd69a4a5e
Jannes Coetzee <jannesrsa@gmail.com>
2020-07-09 12:01:57 +0200

    Add shared material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './shared/material.module';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserAnimationsModule, MaterialModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
