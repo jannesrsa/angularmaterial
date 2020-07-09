c48e0619f4a6b8a098f56f68c786e5c4e5d97d39
Jannes Coetzee <jannesrsa@gmail.com>
2020-07-09 11:11:06 +0200

    Upgrade to Angular Version 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './shared/material.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
