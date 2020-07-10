eed811f73b177dc1082225cf329a8cc06cc454e2
Jannes Coetzee <jannesrsa@gmail.com>
2020-07-10 12:19:59 +0200

    Add FlexBox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
             (mod) => mod.DemoModule
          ),
    },
-   { path: '**', redirectTo: 'contactmanager' }
+   { path: '**', redirectTo: 'demo' }
 ];
 
 @NgModule({
